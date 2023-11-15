44813c81f1d2369a3094c14a64f861e9f65a0106
zy <114321763+zyre4n@users.noreply.github.com>
2023-11-15 21:19:13 +0800

    Update ACK.js

--- ACK.js
+++ ACK.js
@@ -1,8 +1,7 @@
 
 onload = () => {
-    const c = setTimeout(() => {
-      document.body.classList.remove("not-loaded");
-      clearTimeout(c);
-    }, 1000);
-  };
-  
+  const c = setTimeout(() => {
+    document.body.classList.remove("not-loaded");
+    clearTimeout(c);
+  }, 1000);
+};
